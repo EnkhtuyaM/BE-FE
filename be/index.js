14b9ea9617703799e69abbdb36701bd77c5fc1e8
Enkhtuya Myagmar <23LP6946@G401-CC0091.local>
2024-02-21 00:47:39 -0800

    first commit

---
+++ be/index.js
@@ -0,0 +1,67 @@
+const express = require("express");
+const app = express();
+const { products, users } = require("./dummy.json");
+const bodyParser = require("body-parser");
+app.use(bodyParser.json());
+const fs = require("fs");
+// const { error } = require("console");
+
+const cors = require("cors");
+app.use(cors());
+
+app.get("/products", (request, response) => {
+  response.type = "application/json";
+  response.status(200);
+  response.send({ products });
+});
+
+app.get("/users", (request, response) => {
+  response.type = "application/json";
+  response.status(200);
+  response.send({ users });
+});
+
+// app.listen(3001, () => {
+//   console.log("Server is listening");
+// });
+// app.get("/users", (request, response) => {
+//   response.type = "application/json";
+//   response.status(200);
+//   response.send({ name });
+// });
+
+// app.post("/users", (request, response) => {
+//   let data = request.body;
+//   response.send("Data Received:" + JSON.stringify(data));
+// });
+// app.listen(3001, () => {
+//   console.log("Example app listening on port 3001!");
+// });
+
+app.post("/add-user", (req, res) => {
+  const newUser = req.body;
+  console.log(newUser);
+
+  fs.readFile("dummy.json", (error, data) => {
+    if (error) {
+      console.log("Error in reading file");
+    } else {
+      const jsonFile = JSON.parse(data.toString());
+      jsonFile.users.push(newUser);
+      fs.writeFile("dummy.json", JSON.stringify(jsonFile), (err) => {
+        if (err) {
+          console.log(err);
+          res.send("error occured");
+        } else {
+          console.log("success");
+          res.send("User added successfully");
+        }
+      });
+    }
+  });
+});
+// app.post("/read-new-user", (req, res) => {});
+
+app.listen(3001, () => {
+  console.log("Server is listening at port 3001");
+});
---
+++ fe/src/pages/index.js
@@ -0,0 +1,35 @@
+import React from "react";
+
+export default function Home() {
+  const BE_URL = "http://localhost:3001/add-user";
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+    const data = {
+      username: e.target.username.value,
+    };
+
+    const options = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    };
+    const FETCHED_DATA = await fetch(BE_URL, options);
+    const FETCHED_JSON = await FETCHED_DATA.json();
+    console.log("FETCHED_JSON", FETCHED_JSON);
+  }
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <label for="username">
+          Username:
+          <input id="username" name="username" />
+        </label>
+        <input type="submit" value="submit" />
+      </form>
+    </div>
+  );
+}
